7f5aaafca7a9ba75fe2fa2858411570af6e3188e
victor <victor.alexandre@atlantasistemas.com>
2023-07-14 21:29:55 -0300

    Get state manager 2ª forma de nulo pra primitivos

--- lib/tipos/tipos_reativos_page.dart
+++ lib/tipos/tipos_reativos_page.dart
@@ -4,10 +4,14 @@ import 'package:get_state_manager/core/widget/elevated_button_custom.dart';
 
 class TiposReativosPage extends StatelessWidget {
   // Exemplos de Rx
-  final nome = RxString('Victor Alexandre');
-  final isAluno = RxBool(true);
-  final qtdCurso = RxInt(1);
-  final valorCurso = RxDouble(1499.00);
+  final nome = RxString(
+      'Victor Alexandre'); //ou RxnString(), deve usar nome.value para adicionar valor.
+  final isAluno = RxBool(
+      true); // ou RxnBool(), deve usar isAluno.value para adicionar valor.
+  final qtdCurso =
+      RxInt(1); // ou RxnInt(), deve usar qtdCurso.value para adicionar valor.
+  final valorCurso =
+      RxnDouble(); // ou RxnDouble(), deve usar valorCurso.value para adicionar valor.
   final jornadas = RxList([
     'Jornada GetX',
     'Jornada ADF',
